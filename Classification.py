6491a69ed50e3b01f01024e4b0bc494939da3295
ShivaSaiV <106600142+ShivaSaiV@users.noreply.github.com>
2023-05-29 01:36:37 -0400

    Add files via upload

---
+++ Classification.py
@@ -0,0 +1,70 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from IPython.display import clear_output
+from six.moves import urllib
+
+import tensorflow.compat.v2.feature_column as fc
+
+import tensorflow as tf
+
+CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+
+train_path = tf.keras.utils.get_file("iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+test_path = tf.keras.utils.get_file("iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+
+train_data = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+test_data = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+
+y_train = train_data.pop("Species")
+y_test = test_data.pop("Species")
+
+
+def input_fn(features, labels, training=True, batch_size=256):
+    # Convert the inputs to a Dataset.
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle and repeat if you are in training mode.
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+
+    return dataset.batch(batch_size)
+
+feature_column = []
+for key in train_data.keys():
+    feature_column.append(tf.feature_column.numeric_column(key=key))
+
+model = tf.estimator.DNNClassifier(feature_columns=feature_column,
+                                   hidden_units=[30, 10],
+                                   n_classes=3)
+model.train(input_fn=lambda : input_fn(train_data, y_train, training=True),
+            steps=5000)
+result = model.evaluate(input_fn=lambda : input_fn(test_data, y_test, training=False))
+clear_output()
+
+def input_fn(features, batch_size=256):
+    # Convert the inputs to a Dataset without labels.
+    return tf.data.Dataset.from_tensor_slices(dict(features)).batch(batch_size)
+
+features = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
+predict = {}
+
+print("Please type numeric values as prompted.")
+for feature in features:
+  valid = True
+  while valid:
+    val = input(feature + ": ")
+    if not val.isdigit(): valid = False
+
+  predict[feature] = [float(val)]
+
+predictions = model.predict(input_fn=lambda: input_fn(predict))
+for pred_dict in predictions:
+    class_id = pred_dict['class_ids'][0]
+    probability = pred_dict['probabilities'][class_id]
+
+    print('Prediction is "{}" ({:.1f}%)'.format(
+        SPECIES[class_id], 100 * probability))
